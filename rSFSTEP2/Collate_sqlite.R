06918b1f98b5c5a5da898af0bd0cb3bb70a1acda
tmartyn <trace.martyn@yale.edu>
2023-01-24 14:42:13 -0500

    added comments to rcode

--- rSFSTEP2/Collate_sqlite.R
+++ rSFSTEP2/Collate_sqlite.R
@@ -1,5 +1,8 @@
+# written by T. Martyn Jan 2023
+# clear workspace
 rm(list=ls())
-#libraries
+
+# load libraries
 library(DBI)
 library(RSQLite)
 library(rSOILWAT2)
@@ -12,32 +15,39 @@ library(synchronicity)
 sqlite <- dbDriver("SQLite")
 Period<-c("Late","Mid")
 Site<-seq(1:40)
-
+# identify what period ou are working with 
 p<-"Mid"
-  for (s in Site){
-    print(s)
-    database<-paste0("Output/Output_site_",s,".sqlite")
-    mydb<-dbConnect(sqlite,database)
-    biomass.out<-dbGetQuery(mydb,"SELECT * FROM Biomass")
-    biomass.out$RCP<-ifelse(grepl("RCP85",biomass.out$GCM),"RCP85",
+  for (s in Site){ # for each site
+    print(s) #print site 
+    database<-paste0("Output/Output_site_",s,".sqlite") # identify where site is stored on computer
+    mydb<-dbConnect(sqlite,database) # read database
+    biomass.out<-dbGetQuery(mydb,"SELECT * FROM Biomass") # get biomass table
+    # label RCP
+    biomass.out$RCP<-ifelse(grepl("RCP85",biomass.out$GCM),"RCP85", 
                             ifelse(grepl("RCP45",biomass.out$GCM),
                                    "RCP45","CURRENT"))
+    # label GCM
     biomass.out$GCM2<-ifelse(grepl("RCP85",biomass.out$GCM),gsub("RCP85.","",biomass.out$GCM),
                              ifelse(grepl("RCP45",biomass.out$GCM),
                                     gsub("RCP45.","",biomass.out$GCM),"CURRENT"))
+    # label time period
     biomass.out$years<-p
+    
+    # disconnect .sqlite database
     dbDisconnect(mydb)
+    
+    # append dataframe to an 'all' dataframe
     if ("biomass.all" %in% ls()){
       biomass.all<-rbind(biomass.all,biomass.out)
     } else {biomass.all<-biomass.out}
     
   }
-#}
 
+# write large csv
 write.csv(biomass.all,paste0("collate.biomass.output.csv"))
-#rm(biomass.all)
+#rm(biomass.all) # need to remove this if do not clear workspace after forloop.
 
-# 
+# code to look at some graphs of the output
 # library(tidyverse)
 # ggplot(biomass.all,aes(x=Year,y=sagebrush,col=RCP))+
 #   geom_point(alpha=0.2)+
